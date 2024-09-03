f718a214a2265a0330082c43fc9634c23da51ef7
SalvatoreC94 <salvatorecozzolino.1594@gmail.com>
2024-09-03 09:38:16 +0200

    completato1.1

--- js/script.js
+++ js/script.js
@@ -1,1 +1,29 @@
-console.log('Js Cololegato')
+const { createApp } = Vue;
+
+createApp({
+    data() {
+        return {
+            emails: []  // Array per memorizzare le email generate
+        };
+    },
+    created() {
+
+        // Ciclo per generare 10 email
+        for (let i = 0; i < 10; i++) {
+            console.log(`Sto per fare la richiesta numero ${i + 1} all'API.`);
+
+            axios
+                .get('https://flynn.boolean.careers/exercises/api/random/mail')
+                .then((resp) => {
+                    console.log(`Richiesta numero ${i + 1} completata. Email ricevuta:`, resp.data.response);
+
+                    // Aggiunge l'email all'array emails
+                    this.emails.push(resp.data.response);
+                })
+
+
+        }
+
+        console.log('Tutte le richieste API sono state inviate.');
+    }
+}).mount('#app');
